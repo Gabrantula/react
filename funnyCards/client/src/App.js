c392a4d100907577a51826cd357e6e2b40475a43
Gabrantula <104615619+Gabrantula@users.noreply.github.com>
2023-04-07 14:11:43 +0300

    Update App.js

--- funnyCards/client/src/App.js
+++ funnyCards/client/src/App.js
@@ -1,63 +1,65 @@
 
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
-import Gaby from './components/Gaby';
+import Gaby from './components/Gaby/Gaby';
 import Catalog from './components/Catalog/Catalog';
-import Create from './components/Create';
-import Details from './components/Details';
-import Edit from './components/Edit';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import Login from './components/Login';
-import MyPage from './components/MyPage';
-import Navigation from './components/Navigation';
-import Register from './components/Register';
+import Create from './components/Create/Create';
+import Details from './components/Details/Details';
+import Edit from './components/Edit/Edit';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import MyPage from './components/MyPage/MyPage';
+import Navigation from './components/Navigation/Navigation';
+import Register from './components/Register/Register';
+import Logout from './components/Logout/Logout'
+import NotFound from './components/404/NotFound';
 
-import { useState, useEffect } from "react";
-import * as CardService from './services/CardService'
+import { AuthProvider } from './contexts/AuthContext'
+import { CardProvider } from './contexts/CardContext';
+import { RouteGuard } from './components/common/RouteGuard';
+import Comments from './components/Comments/Comments';
+import CardOwner from './components/common/CardOwner'
+
 
 function App() {
-  const [cards, setCards] = useState([])
-  const navigate =useNavigate()
 
-  useEffect(() => {
-     CardService.getAll()
-     .then(result => {
-       //console.log(result);
-       setCards(result)
-     })
-  },[])
-
-  const onCreateCardSubmit = async(data) => {
-   
-    const newCard = await CardService.create(data)
-    //console.log(newGame);
-    setCards(state => [...state, newCard])
-    navigate('/all-cards')
-    
-     }
   return (
-    <div className="App">
-      <header>
-        <Navigation />
-      </header>
-      <main id="main-container">
-        <Routes>
+    <AuthProvider>
+      <CardProvider>
+        <div className="App">
+          <header>
+            <Navigation />
+          </header>
+          <main id="main-container">
+            <Routes>
+              <Route path='*' element={<NotFound />} />
+              <Route path='/' element={<Home />} />
+              <Route path='/all-cards' element={<Catalog />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/Gaby-page' element={<Gaby />} />
+              <Route path='/my-cards' element={<MyPage />} />
+              <Route path='/all-cards/:cardId' element={<Details />} />
 
-          <Route path='/' element={<Home />} />
-          <Route path='/all-cards' element={<Catalog cards={cards} />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/create' element={<Create onCreateCardSubmit={onCreateCardSubmit} />} />
-          <Route path='/Gaby-page' element={<Gaby />} />
-          <Route path='/my-cards' element={<MyPage />} />
-          <Route path='/details/:cardId' element={<Details />} />
-          <Route path='/edit' element={<Edit />} />
-        </Routes>
+              <Route element={<RouteGuard />} >
+                <Route path='/create' element={<Create />} />
+                <Route path='/all-cards/:cardId/edit' element={
+                  <CardOwner>
+                    <Edit />
+                  </CardOwner>
+                } />
+                <Route path='/logout' element={<Logout />} />
+                <Route path='/all-cards/:cardId/comments' element={<Comments />} />
+              </Route>
 
-        <Footer />
-      </main>
-    </div>
+            </Routes>
+
+            <Footer />
+          </main>
+        </div>
+      </CardProvider>
+    </AuthProvider>
   );
 }
 
