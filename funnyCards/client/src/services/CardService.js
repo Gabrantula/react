3d670ef6feccb3b96fc5f98b54a04a7384b1c62c
Gabrantula <104615619+Gabrantula@users.noreply.github.com>
2023-04-07 14:09:01 +0300

    Update CardService.js

--- funnyCards/client/src/services/CardService.js
+++ funnyCards/client/src/services/CardService.js
@@ -1,20 +1,44 @@
+import { RequestFactory } from "./Requester"
 
-import * as Request from "./Requester"
 const baseUrl = 'http://localhost:3030/data/cards'
 
-export const getAll = async () => {
 
-    const result = await Request.get(baseUrl)
-    const cards = Object.values(result)
+//export const CardServiceFactory = () => {
+   // const Request = RequestFactory()
+export const CardServiceFactory = (token) => {
+    const Request = RequestFactory(token)
+   
+    const getAll = async () => {
 
-    return cards
-}
-export let getOne = async ( cardId) => {
-    let result = await Request.get(`${baseUrl}/${cardId}`)
-    return result
-}
-export const create = async (cardData) => {
-    const result = await Request.post(baseUrl, cardData)
-   // console.log(result);
-    return result
+        const result = await Request.get(baseUrl)
+        const cards = Object.values(result)
+
+        return cards
+    }
+    let getOne = async (cardId) => {
+        let result = await Request.get(`${baseUrl}/${cardId}`)
+        return result
+    }
+    const create = async (cardData) => {
+        const result = await Request.post(baseUrl, cardData)
+
+        return result
+    }
+   
+    const edit = async(cardId, data) => {
+       const result= await Request.put(`${baseUrl}/${cardId}`, data)
+       return result
+    }
+    const deleteCard = (cardId) => {
+      const result=  Request.del(`${baseUrl}/${cardId}`)
+      return result
+    }
+   
+    return {
+        getAll,
+        getOne,
+        create,
+        edit,
+        deleteCard, 
+    }
 }
